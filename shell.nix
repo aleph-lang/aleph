78ff2df673b527e3cc4188ced1e5cc1079d11f4a
jsz4n <jsz4n@laposte.net>
2022-07-10 15:10:40 +0200

    add shell nix file

---
+++ shell.nix
@@ -0,0 +1,10 @@
+let pkgs = import <nixpkgs> {};
+# choose the ocaml version you want to use
+ocamlPackages = pkgs.ocaml-ng.ocamlPackages_4_12;
+in
+pkgs.mkShell {
+    # build tools
+    nativeBuildInputs = with ocamlPackages; [ ocaml ]; #ocaml-lsp ];
+    # dependencies
+    buildInputs = with pkgs; [ ocaml_make ] ;
+}
