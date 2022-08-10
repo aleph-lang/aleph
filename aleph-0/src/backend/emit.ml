814d96b85d1662cc0d86c8856b9526531311b7e2
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-08-10 12:59:58 +0200

    add if generation

--- aleph-0/src/backend/emit.ml
+++ aleph-0/src/backend/emit.ml
@@ -22,7 +22,7 @@ let rec g env = function
   | FMul(x, y) -> (g env x) ^ " *. " ^ (g env y)
   | FDiv(x, y) -> (g env x) ^ " /. " ^ (g env y)
   | Let((x, t), e1, e2) -> " let " ^ x ^ " = " ^ (g env e1) ^" in "^ (g (M.add x t env) e2)
-  | If(e1, e2, e3) -> (g env e1) ^ " then " ^ (g env e2) ^ " else " ^ (g env e3)
+  | If(e1, e2, e3) -> "if " ^ (g env e1) ^ " then " ^ (g env e2) ^ " else " ^ (g env e3)
   | While(e1, e2, e3, e4) ->  (g env e1) ^ (g env e2) ^ (g env e3) ^ (g env e4)
   | Var(x) -> x
   | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> x ^ (g env e1) ^ (g env e2)
