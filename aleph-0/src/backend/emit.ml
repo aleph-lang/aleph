1a9b854fe0d02d102863468456edb4a890ac7068
jsz4n <3914641+jsz4n@users.noreply.github.com>
2022-08-10 19:51:34 +0200

    while ocaml syntax

--- aleph-0/src/backend/emit.ml
+++ aleph-0/src/backend/emit.ml
@@ -23,7 +23,7 @@ let rec g env = function
   | FDiv(x, y) -> (g env x) ^ " /. " ^ (g env y)
   | Let((x, t), e1, e2) -> " let " ^ x ^ " = " ^ (g env e1) ^" in "^ (g (M.add x t env) e2)
   | If(e1, e2, e3) -> "if " ^ (g env e1) ^ " then " ^ (g env e2) ^ " else " ^ (g env e3)
-  | While(e1, e2, e3, e4) ->  (g env e1) ^ (g env e2) ^ (g env e3) ^ (g env e4)
+  | While(e1, e2, e3, e4) ->   (g env e1) ^ "while "^ (g env e2) ^ " do  " ^ (g env e3) ^ ";" ^ (g env e4) ^ " done"
   | Var(x) -> x
   | LetRec({ name = (x, t); args = yts; body = e1 }, e2) -> x ^ (g env e1) ^ (g env e2)
   | App(x, xs) -> (g env x) ^ "(" ^ (String.concat " " (List.map (g env) xs)) ^ ")\n"
