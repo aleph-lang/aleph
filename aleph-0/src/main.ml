0f42e9609342c35d7803775c47239b4657314fb0
Steve Roques <steve.roques@gmail.com>
2022-08-20 22:09:16 +0200

    configuration file prepared

--- aleph-0/src/main.ml
+++ aleph-0/src/main.ml
@@ -1,6 +1,30 @@
+let confFile = ref ""
+
+let read_file filename = 
+let lines = ref [] in
+let chan = open_in filename in
+try
+  while true; do
+    lines := input_line chan :: !lines
+  done; !lines
+with End_of_file ->
+  close_in chan;
+  List.rev !lines ;;
+
 let lexbuf outchan l =
   Id.counter := 0;
-  let res = Ocaml.f (Parser.exp Lexer.token l) in
+  let process = 
+  (if !confFile = ""
+  then begin
+    Printf.printf "Configuraiton file : Default (Al0 -> Ocaml)\n";
+    Ocaml.f
+  end else begin
+    Printf.printf "Configuraiton file : %s\n" !confFile;
+    let lines = read_file !confFile in
+    Printf.printf "%s\n" (String.concat " " lines);
+    Ocaml.f
+  end) in
+  let res = process (Parser.exp Lexer.token l) in
   Printf.fprintf outchan "%s\n" res
 
 let file f =
@@ -15,7 +39,7 @@ let file f =
 let () =
   let files = ref [] in
   Arg.parse
-    []
+    [("-conf", Arg.String(fun s -> confFile := s), "Compiler configuration file")]
     (fun s -> files := !files @ [s])
     ("Aleph-0 compiler\n" ^
      Printf.sprintf "usage: %s filenames" Sys.argv.(0));
