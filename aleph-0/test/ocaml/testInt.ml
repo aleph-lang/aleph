2834ceb514a66b32c8131e7f5fb1b0c22a9482c3
Steve Roques <steve.roques@gmail.com>
2022-08-26 19:53:15 +0200

    prepare parser ocaml

---
+++ aleph-0/test/ocaml/testInt.ml
@@ -0,0 +1,7 @@
+Printf.printf "%d\n" 9;
+Printf.printf "%d\n" (3 + 4);
+Printf.printf "%d\n" (2 * 5);
+Printf.printf "%d\n" (6 / 8);
+Printf.printf "%d\n" (9 - 5);
+Printf.printf "%d\n" (3 - 8);
+Printf.printf "%d\n" (-6);;
