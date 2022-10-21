3913c45aea72de70eaa63d802fc17f603fbe7fd6
Steve Roques <steve.roques@gmail.com>
2022-10-21 13:17:51 +0200

    float, bool and basic operators

--- src/filter/parser/ale/mod.rs
+++ src/filter/parser/ale/mod.rs
@@ -9,17 +9,31 @@ pub mod grammar {
     #[rust_sitter::language]
     #[derive(Debug)]
     pub enum Expression {
+        Bool(#[rust_sitter::leaf(pattern = "true|false", transform = |v| v.parse().unwrap())] bool),
         Number(#[rust_sitter::leaf(pattern = r"\d+", transform = |v| v.parse().unwrap())] i32),
+        Float(#[rust_sitter::leaf(pattern = r"\d*\.\d*",transform = |v| v.parse().unwrap())] f32),
         #[rust_sitter::prec_left(1)]
+        Add(
+            Box<Expression>,
+            #[rust_sitter::leaf(text = "+")] (),
+            Box<Expression>,
+        ),
+        #[rust_sitter::prec_left(2)]
         Sub(
             Box<Expression>,
             #[rust_sitter::leaf(text = "-")] (),
             Box<Expression>,
         ),
-        #[rust_sitter::prec_left(2)]
+        #[rust_sitter::prec_left(3)]
         Mul(
             Box<Expression>,
             #[rust_sitter::leaf(text = "*")] (),
+            Box<Expression>,
+        ),
+        #[rust_sitter::prec_left(4)]
+        Div(
+            Box<Expression>,
+            #[rust_sitter::leaf(text = "/")] (),
             Box<Expression>,
         ),
     }
@@ -33,9 +47,13 @@ pub mod grammar {
 
 fn translate(tree : grammar::Expression) -> at {
      match tree {
+         grammar::Expression::Bool(b) => at::Bool{value: b.to_string()},
          grammar::Expression::Number(i) => at::Int{value: i.to_string()},
+         grammar::Expression::Float(f) => at::Float{value: f.to_string()},
+         grammar::Expression::Add(e1,_, e2) => at::Add{number_expr1 : Box::new(translate(*e1)), number_expr2: Box::new(translate(*e2))},
          grammar::Expression::Sub(e1,_, e2) => at::Sub{number_expr1 : Box::new(translate(*e1)), number_expr2: Box::new(translate(*e2))},
-         _ => at::Int{value: "-1".to_string()}
+         grammar::Expression::Mul(e1,_, e2) => at::Mul{number_expr1 : Box::new(translate(*e1)), number_expr2: Box::new(translate(*e2))},
+         grammar::Expression::Div(e1,_, e2) => at::Div{number_expr1 : Box::new(translate(*e1)), number_expr2: Box::new(translate(*e2))}, 
      }
 }
 
