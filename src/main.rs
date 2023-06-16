5f3762a097ebdd0395f37f75d630e6f00689f3f4
Roques Steve <steve.roques@gmail.com>
2023-06-16 14:27:07 +0200

    remove conf reader

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
 extern crate argparse;
 
-use argparse::{ArgumentParser, Store, StoreTrue};
+use argparse::{ArgumentParser, Store};
 use std::io;
 use std::io::Read;
-use std::fs;
 
 mod filter;
 use crate::filter::generate;
@@ -12,29 +11,21 @@ fn main() {
     let mut content_type = "ale".to_string();
     let mut return_type = "ale".to_string();
     let mut transformer_param = "".to_string();
-    let mut infos = false;
     {  
         let mut ap = ArgumentParser::new();
         ap.set_description("Multi compiler");
         ap.refer(&mut content_type).add_option(&["--in_type", "-i"], Store, "Input type");
         ap.refer(&mut transformer_param).add_option(&["--transformer_list", "-t"], Store, "Transformer list");
         ap.refer(&mut return_type).add_option(&["--out_type", "-o"], Store, "Output type");
-        ap.refer(&mut infos).add_option(&["-g", "--infos"], StoreTrue, "Get informations");
         ap.parse_args_or_exit();
     }
 
-    let response = if !infos {
-        let transformer_list : Vec<String> = transformer_param.split(",").map(|s| s.to_string()).collect();
+    let transformer_list : Vec<String> = transformer_param.split(",").map(|s| s.to_string()).collect();
  
-        let mut content = String::new();
-        io::stdin().read_to_string(&mut content).unwrap();
+    let mut content = String::new();
+    io::stdin().read_to_string(&mut content).unwrap();
      
-        generate(content_type, content, Some(transformer_list), return_type)
-    } else {
-        let contents = fs::read_to_string("conf/default.conf")
-        .expect("Should have been able to read the file");
-        contents
-    };
+    let response = generate(content_type, content, Some(transformer_list), return_type);
     
     println!("{}", response);
 }
